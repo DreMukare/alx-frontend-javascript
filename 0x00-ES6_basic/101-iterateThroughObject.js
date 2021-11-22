6b86ecbe2f468584d44e1c7c28500c793a311f3b
JudgeFudge19 <dremukare@gmail.com>
2021-11-22 16:08:02 +0300

    feat: done second advanced task

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,14 @@
+export default function iterateThroughObject(reportWithIterator) {
+	let i = 0;
+	let output = '';
+	for (let value in reportWithIterator) {
+		if (i !== reportWithIterator.length - 1) {
+			output += `${reportWithIterator[value]} | `;
+		} else {
+			output += reportWithIterator[value];
+		}
+		i++;
+	}
+
+	return output;
+}
