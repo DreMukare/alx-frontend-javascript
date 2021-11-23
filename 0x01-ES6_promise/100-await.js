5f22573cdc62f6d00a4396639c1584bcf6e0c299
Andrew Mukare <dremukare@gmail.com>
2021-11-23 20:07:12 +0000

    feat: done advanced task

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,18 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const user = await createUser();
+    const photo = await uploadPhoto();
+
+    return {
+      photo,
+      user,
+    };
+  } catch (e) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
