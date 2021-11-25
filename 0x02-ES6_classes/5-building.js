adfbff7f29881091a3c62ad5444b186ec0af6876
Andrew Mukare <dremukare@gmail.com>
2021-11-25 12:57:19 +0000

    feat: done task 6 and attempted different solution for task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -4,14 +4,14 @@ export default class Building {
       throw new TypeError('Sqft must be a number');
     }
 
-    if (new.target.evacuationWarningMessage === undefined) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
-
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
