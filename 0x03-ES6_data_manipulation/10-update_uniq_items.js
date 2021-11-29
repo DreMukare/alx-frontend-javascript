6de4939aceec460cca38afb756b741c630c7b005
Andrew Mukare <dremukare@gmail.com>
2021-11-29 10:08:31 +0000

    feat: done task 10

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,10 @@
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) throw new Error('Cannot process');
+
+  for(let item of map) {
+    if (item[1] === 1) {
+      map.set(item[0], 100);
+    }
+  }
+  return map;
+}
