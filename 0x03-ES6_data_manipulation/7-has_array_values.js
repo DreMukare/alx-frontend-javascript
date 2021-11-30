64e6af166b269564c385241bac64d785e462b0aa
Andrew Mukare <dremukare@gmail.com>
2021-11-30 06:37:47 +0000

    bug(task 7): fixed lint errors

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,8 +1,8 @@
 export default function hasValuesFromArray(set, array) {
   if (!set || !Array.isArray(array)) throw new Error('invalid parameter');
   let output = true;
-  for (let i = 0; i < array.length; i++) {
-    set.has(array[i]) ? output = true : output = false;
+  for (let i = 0; i < array.length; i += 1) {
+    output = set.has(array[i]);
   }
   return output;
 }
