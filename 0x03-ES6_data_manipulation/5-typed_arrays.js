1fc844f196e80855c5a88533c3d0b03344dc5e2a
Andrew Mukare <dremukare@gmail.com>
2021-11-30 06:47:11 +0000

    bug(task 5): changed to required error message

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
   if (typeof length !== 'number' || typeof position !== 'number' || typeof value !== 'number') {
-    throw new TypeError('Position outside range');
+    throw new Error('Position outside range');
   }
   const buffer = new ArrayBuffer(length);
   const int8View = new Int8Array(buffer);
