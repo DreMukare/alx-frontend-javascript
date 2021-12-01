9a8c2365ca9a77dd981efea2a238464bfcf2ff76
Andrew Mukare <dremukare@gmail.com>
2021-12-01 21:42:35 +0000

    feat: done task 1 - 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -5,4 +5,41 @@ interface Teacher {
   yearsOfExperience?: number;
   location: string;
   [propName: string]: any;
- }
+}
+
+interface Director extends Teacher {
+  numberOfReports: number;  
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+function printTeacher (firstName: string, lastName: string):string {
+  return `${firstName.charAt(0)}. ${lastName}`;
+}
+
+interface Student {
+  firstName: string;
+  lastName: string;
+  workOnHomework:() => string;
+  displayName:() => string;
+}
+
+class StudentClass implements Student {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework():string {
+    return 'Currently working';
+  }
+
+  displayName():string {
+    return this.firstName;
+  }
+}
